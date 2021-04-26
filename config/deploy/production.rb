6b22b7d727fbdb32be816b9f5818d944146e5a54
Ngoc Bach <ngocbv94@gmail.com>
2021-04-26 20:44:04 +0700

    Add production key

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -6,3 +6,4 @@ set :branch, 'master'
 
 # Setup IP with ec2 server
 server '18.220.8.150', user: fetch(:user), roles: %w[app web db]
+set :ssh_options, { forward_agent: true, user: fetch(:user), keys: '/Users/macOS/.ssh/binhtl.pem' }
