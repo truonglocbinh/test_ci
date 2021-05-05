2647bf4358f32721b315598b051653ff8741e7d2
Ngoc Bach <ngocbv94@gmail.com>
2021-05-05 21:24:48 +0700

    Fix validate

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,6 +1,6 @@
 class Book < ApplicationRecord
   paginates_per 50
 
-  validates :title, presence: true, length: { maximum: 20 }
+  validates :title, presence: true, length: { maximum: 100 }
   validates :author, presence: true, length: { maximum: 225 }
 end
